a4144ffbdbe8ae5202219dcfd95026f7130c209e
fangping <344602831@qq.com>
2016-12-07 15:42:06 +0800

    测试上传项目到 gethub 服务器

---
+++ src/MyTest/HelloWorld.java
@@ -0,0 +1,12 @@
+package MyTest;
+
+public class HelloWorld {
+	
+	public static void main(String[] args)
+	{
+		//http://blog.csdn.net/boybruce/article/details/40598083
+		System.out.println("Hello Wworld for GitHub !!!!");
+		// add form local
+		//System.out.println("Add form local");
+	}
+}
