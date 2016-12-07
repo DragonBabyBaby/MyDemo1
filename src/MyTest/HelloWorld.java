1d18a55a09a1aa31c4dc8def91ea10b694ca80ca
fangping <344602831@qq.com>
2016-12-07 16:11:59 +0800

    更新了HelloWorld。java方法

--- src/MyTest/HelloWorld.java
+++ src/MyTest/HelloWorld.java
@@ -7,6 +7,6 @@ public class HelloWorld {
 		//http://blog.csdn.net/boybruce/article/details/40598083
 		System.out.println("Hello Wworld for GitHub !!!!");
 		// add form local
-		//System.out.println("Add form local");
+		System.out.println("Add form local");
 	}
 }
